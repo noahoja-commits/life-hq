833fe76c9f44278f49b27c859d9804eea25832e7
noahoja-commits <noahoja@gmail.com>
2026-07-06 05:33:40 -0400

    art: giant demonic eye on dashboard (400px, 6% opacity), SectionArt banner on dashboard

--- src/components/atomic-crm/dashboard/Dashboard.tsx
+++ src/components/atomic-crm/dashboard/Dashboard.tsx
@@ -13,6 +13,7 @@ import { PushReminders } from "../push/PushReminders";
 import { TodayView } from "./TodayView";
 import { ActivityFeed } from "./ActivityFeed";
 import { DemonicEye } from "../misc/DemonicEye";
+import { SectionArt } from "../misc/SectionArt";
 import type { Deal } from "../types";
 
 interface HubItem {
@@ -94,7 +95,8 @@ export const Dashboard = () => {
 
   return (
     <div data-section="dashboard" className="mx-auto flex max-w-5xl flex-col gap-7 py-2 page-enter stagger relative">
-      <DemonicEye size={320} className="absolute -top-20 -right-20 opacity-[0.025] pointer-events-none rotate-12" />
+      <SectionArt section="dashboard" />
+      <DemonicEye size={400} className="absolute -top-20 -right-20 opacity-[0.06] pointer-events-none rotate-12" />
       {/* Operations header: greeting left, capture right */}
       <div className="flex flex-wrap items-end justify-between gap-x-6 gap-y-3">
         <div className="min-w-0">
