fa78b35641614ec9fbb31e07965414bea85a3aaa
noahoja-commits <noahoja@gmail.com>
2026-07-07 00:06:41 -0400

    cleanup: remove temp scripts

--- fix_scroll.mjs
+++
@@ -1,8 +0,0 @@
-import { readFileSync, writeFileSync } from "fs";
-const f = readFileSync("src/components/atomic-crm/layout/Layout.tsx", "utf8");
-const f2 = f
-  .replace("page-enter ", "")
-  .replace("import { useLocation } from 'react-router';\n", "")
-  .replace("const location = useLocation();\n", "");
-writeFileSync("src/components/atomic-crm/layout/Layout.tsx", f2, "utf8");
-console.log("fixed: removed page-enter, useLocation");
